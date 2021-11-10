ebd1e30cf2a27c48550c49771ea146333a76bdf5
tarushi98 <tarushipathak@gmail.com>
2021-11-10 22:35:55 +0530

    navbar change

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const ProfileImage = () => (
 const NavBar=()=>
 (<>
     <div className="flex flex-row justify-between mt-8">
-      <nav className="flex-none flex-row space-x-4">
+      <nav className="flex flex-row space-x-4">
           <button className="visible border-4 rounded-lg pl-2 pt-2 pb-2 hover:border-black sm:hidden"><img src="https://img.icons8.com/windows/32/000000/menu.png" width="75%"/></button>
           <Link href="/" className="active:text-black"><p className="hidden text-2xl text-gray-500 hover:underline hover:text-black sm:block">Home</p></Link>
           <p className="hidden text-2xl text-gray-500 hover:underline hover:text-black sm:block">About</p>
