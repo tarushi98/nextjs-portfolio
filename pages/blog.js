efe096af01299a8c36c9db60325614968a56d122
tarushi98 <tarushipathak@gmail.com>
2021-12-14 22:25:52 +0530

    blog rechecks

--- pages/blog.js
+++ pages/blog.js
@@ -41,20 +41,20 @@ const Blogs = () => (
               </div>
         </div>
         <p className="font-bold text-4xl mt-10 sm:ml-6">TECH BLOGS</p>
-        <div className="flex flex-col space-y-8 mt-5 sm:ml-8">
-                <div className="flex flex-row space-x-10 sm:space-x-36">
+        <div className="flex flex-col space-y-8 mt-5 sm:ml-12">
+                <div className="flex flex-row space-x-10 sm:space-x-64">
                         <Link href="https://medium.com/analytics-vidhya/classifying-asteroids-using-ml-a-beginners-tale-part-1-f4385458f13"><p className="text-md sm:text-2xl hover:underline">Classifying Asteroids Using ML (Part 1)<br/><span className="italic text-gray-500 text-md">Machine Learning,Astronomy</span></p></Link>
                         <p className="text-md sm:text-2xl">Jul 12,2020</p>
                 </div>
-                <div className="flex flex-row space-x-10 sm:space-x-36">
+                <div className="flex flex-row space-x-10 sm:space-x-64">
                         <Link href="https://medium.com/analytics-vidhya/classifying-asteroids-using-ml-a-beginners-tale-part-2-1e379f7a781d"><p className="text-md sm:text-2xl hover:underline">Classifying Asteroids Using ML (Part 2)<br/><span className="italic text-gray-500 text-md">Machine Learning,Astronomy</span></p></Link>
                         <p className="text-md sm:text-2xl">Jul 13,2020</p>
                 </div>
-                <div className="flex flex-row space-x-10 sm:space-x-56">
+                <div className="flex flex-row space-x-10 sm:space-x-44">
                         <Link href="https://medium.com/data-science-community-srm/from-50-to-5000-an-image-augmentation-story-1fc30111e39"><p className="text-md sm:text-2xl hover:underline">From 50 to 5000, an Image Augmentation Story<br/><span className="italic text-gray-500 text-md">Deep Learning</span></p></Link>
                         <p className="text-md sm:text-2xl">Jul 23,2020</p>
                 </div>
-                <div className="flex flex-row space-x-10 sm:space-x-60">
+                <div className="flex flex-row space-x-10 sm:space-x-48">
                         <Link href="https://medium.com/@tarushipathak/my-open-source-journey-with-student-code-in-51195dad9d0e"><p className="text-md sm:text-2xl hover:underline">My Open Source Journey with Student Code-IN<br/><span className="italic text-gray-500 text-md">Open Source,Deep Learning</span></p></Link>
                         <p className="text-md sm:text-2xl">Sep 1,2020</p>
                 </div>
